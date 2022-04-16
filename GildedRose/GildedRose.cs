5b5bfd58971ef2c2754497b0daf546edc2425eb2
Alex Adamovic <alexanderadamovic@gmail.com>
2022-04-16 15:59:51 -0700

    Create seperate changeQuality method

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -21,92 +21,48 @@ namespace GildedRoseKata
         }
         private void UpdateItem(Item item)
         {
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
-            {
-                if (item.Quality > 0)
-                {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            if (item.Quality < 50)
-                            {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
             if (item.Name != "Sulfuras, Hand of Ragnaros")
             {
                 item.SellIn = item.SellIn - 1;
             }
 
-            if (item.SellIn < 0)
-            {
-                if (item.Name != "Aged Brie")
-                {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-            }
+            changeQuality(item);
         }
 
         private void changeQuality(Item item)
         {
-            if (item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert") 
+            if ((item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert") && item.Quality < 50) 
             {
                 item.Quality += 1;
             }
-            if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 11)
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 11 && item.Quality < 50)
             {
                 item.Quality += 1;
             }
-            if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 6)
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 6 && item.Quality < 50)
             {
                 item.Quality += 1;
             }
-            if (item.)
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.SellIn < 1)
+            {
+                item.Quality = 0;
+            }
+            if (item.Name == "Conjured Mana Cake" && item.Quality < 0)
+            {
+                item.Quality -= 2;
+            }
+            if (item.Name == "Conjured Mana Cake" && item.SellIn <= 0 && item.Quality < 0)
+            {
+                item.Quality -= 2;
+            }
+            if ((item.Name != "Conjured Mana Cake" || item.Name != "Aged Brie" || item.Name != "Backstage passes to a TAFKAL80ETC concert" || item.Name != "Sulfuras, Hand of Ragnaros") && item.Quality < 0)
+            {
+                item.Quality -= 1;
+            }
+            if ((item.Name != "Conjured Mana Cake" || item.Name != "Aged Brie" || item.Name != "Backstage passes to a TAFKAL80ETC concert" || item.Name != "Sulfuras, Hand of Ragnaros") && item.Quality < 0 && item.SellIn < 1)
+            {
+                item.Quality -= 1;
+            }
         }
     }
 }
