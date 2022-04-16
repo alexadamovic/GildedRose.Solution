7035273ce4beeffab2186b8775d4023718bbe95c
Alex Adamovic <alexanderadamovic@gmail.com>
2022-04-16 13:48:14 -0700

    Add Item property constructor tests

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -11,8 +11,23 @@ namespace GildedRoseTests
         {
             IList<Item> Items = new List<Item> { new Item { Name = "name", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
             Assert.Equal("name", Items[0].Name);
+        }
+
+        [Fact]
+        public void constructsItemWithSellIn()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "name", SellIn = 3, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            Assert.Equal(3, Items[0].SellIn);
+        }
+
+        [Fact]
+        public void constructsItemWithQuality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "name", SellIn = 0, Quality = 3 } };
+            GildedRose app = new GildedRose(Items);
+            Assert.Equal(3, Items[0].Quality);
         }
     }
 }
